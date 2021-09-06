ac2530f087af5a18d22446c9ae70aabe371c0b73
Maksim Kalugin <wolfqest24@gmail.com>
2021-09-07 00:04:56 +0500

    HW python first added

---
+++ First_HW.py
@@ -0,0 +1,47 @@
+#First 10 tasks HW
+a = 24
+print ("Тип переменной a,  имеющей значение", a, "is", type (a))
+b = 24.25645
+print ("Тип переменной b, имеющей значение", b, "is", type (b))
+c = "Hello_World"
+print ("Тип переменной c, имеющей значение", c, "is", type (c))
+d = bytes()
+print ("Тип переменной d, имеющей значение", d, "is", type (d))
+e = [1,2,3]
+print ("Тип переменной e, имеющей значение", e, "is", type (e))
+f = 24,25
+print ("Тип переменной f, имеющей значение", f, "is", type (f))
+g = set()
+print ("Тип переменной g, имеющей значение", g, "is", type (g))
+h = frozenset()
+print ("Тип переменной h, имеющей значение", h, "is", type (h))
+i = {}
+print ("Тип переменной i, имеющей значение", i, "is", type (i))
+
+#11 task
+S1= 'Hello'
+S2 = ' World'
+S3= S1 + S2
+print(S3)
+
+#12 task
+j = 19
+k = 5
+jk = j + k
+print(jk)
+
+#13 task
+dif = j / k
+print(dif)
+
+#14 task
+mult = j * k
+print(mult)
+
+#15 task
+Delbost = j // k
+print(Delbost)
+
+#16 task
+Delsost = j % k
+print(Delsost)
